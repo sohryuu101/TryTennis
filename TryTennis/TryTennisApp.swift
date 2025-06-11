a82de5af0aceade6c5414a5e421dbd7a4a522cd6
RichSvK <richardsugiharto0@gmail.com>
2025-06-11 16:10:50 +0700

    Initial Commit

---
+++ TryTennis/TryTennisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryTennisApp.swift
+//  TryTennis
+//
+//  Created by Richard Sugiharto on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TryTennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
